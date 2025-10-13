59b1320ae136da63c430d175de8ada86162ca710
Arthur Willers <arthur2008willers@gmail.com>
2025-10-13 19:50:14 -0300

    Commit inicial do projeto Aurum em Java

---
+++ src/aurum/java/Aurum.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aurum.java;
+
+/**
+ *
+ * @author ArthurWillers
+ */
+public class Aurum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
